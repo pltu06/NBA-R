a1f4e91938aff59ec699d15d0fffebbb1ec96e94
Amy Bohnert Tu <amybohnerttu@amys-mbp.lan>
2021-11-28 18:02:11 -0600

    Made progress on analysis and scripting

---
+++ R-NBA-Prepro.R
@@ -0,0 +1,18 @@
+#R NBA Preprocessing
+#Patrick Tu
+#Started on 11/28/21
+#Purpose: Generate data frames for analysis
+
+#Preparing the workspace----
+source("R-Prep.R")
+
+##This is all the regular season game logs from 2000 to 2021
+years <- seq(1990, 2021, 1)
+rs_game_logs <- 
+  game_logs(
+    seasons = years, 
+    league = "NBA", 
+    result_types = "team", 
+    season_types = "Regular Season"
+    ) 
+save(rs_game_logs, file = "Output/rs_game_logs.RData")
